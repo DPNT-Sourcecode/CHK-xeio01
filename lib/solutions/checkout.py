9492ac6d4c62c47526f7af71787b91cf7b7c1c03
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 15:57:14 +0000

    Tue May 08 15:57:14 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,13 +5,32 @@
 def checkout(skus):
     valid_skus = ['A', 'B', 'C', 'D']
 
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15,
+    }
+
+    promotions = {
+        'A': {
+            'multiplier': 3,
+            'amount': 130,
+        },
+        'B': {
+            'multiplier': 2,
+            'amount': 45,
+        },
+    }
+
     # assuming it's 4A not AAAA
 
     basket = {}
 
-    for i in range(0, len(skus, 2)):
+    for i in range(len(skus)):
+        if skus[i]
         basket[skus[i+1]] = skus[i]
     
-    if 
+    amount = 0
 
     return -1
