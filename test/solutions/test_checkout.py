70f02bd708a9a9aeaaff028d98561fb66a4c3b4c
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 15:51:14 +0000

    Tue May 08 15:51:14 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -5,9 +5,25 @@ from lib.solutions.checkout import checkout
 
 class TestCheckout(unittest.TestCase):
     def test_illegal_input_returns_-one(self):
-        self.fail()
+        self.assertEqual(
+            -1,
+            checkout('K')
+        )
 
-    def test_a_promo
+    def test_promo_applied_to_a(self):
+        self.assertEqual(
+            260,
+            checkout('AAAAAA')  # ?? 6A or 6 As?
+        )
+
+    def test_promo_applied_to_b(self):
+        # self.fail()
+
+    def test_promo_applied_to_a_and_b(self):
+        # self.fail()
+    
+    def test_valid_input_returns_correct_amount(self):
+        # self.fail()
 
 
 if __name__ == '__main__':
