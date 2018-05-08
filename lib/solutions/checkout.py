32496503476ba0cd78d08530d0526b141d2cc4f8
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 16:01:38 +0000

    Tue May 08 16:01:38 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -36,11 +36,11 @@ def checkout(skus):
     amount = 0
 
     for item in basket:
+        non_promo_number = 0
         if item in promotions:
             non_promo_number = basket[item] % promotions[item]['multiplier']
-            amount += (
-                non_promo_number*prices[item] 
-                + (basket[item] - non_promo_number)*promotions[item]['amount']
-            )
+            amount += (basket[item] - non_promo_number)*promotions[item]['amount']
+        
+        amount += non_promo_number*prices[item]
 
-    return -1
+    return amount
