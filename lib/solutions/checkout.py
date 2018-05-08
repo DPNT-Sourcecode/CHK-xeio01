fbaeef3a54df0d6d9d80b456860b89ee132743a7
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 15:54:14 +0000

    Tue May 08 15:54:14 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    valid_skus = ['A', 'B', 'C', 'D']
+
+    # assuming it's 4A not AAAA
+
+    basket = {}
+
+    for i in range(0, len(skus, 2)):
+        basket[skus[i+1]] = skus[i]
+    
+    if 
+
+    return -1
