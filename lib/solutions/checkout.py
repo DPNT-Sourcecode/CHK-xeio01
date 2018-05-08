0961a12d9b64cf09d366735d1ca01e1912370b52
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 16:07:00 +0000

    Tue May 08 16:07:00 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -40,7 +40,7 @@ def checkout(skus):
     amount = 0
 
     for item in basket:
-        non_promo_number = 0
+        non_promo_number = basket[item]
         if item in promotions:
             non_promo_number = basket[item] % promotions[item]['multiplier']
             amount += (basket[item] - non_promo_number)*promotions[item]['amount']
--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -4,7 +4,7 @@ from lib.solutions.checkout import checkout
 
 
 class TestCheckout(unittest.TestCase):
-    def test_illegal_input_returns_-one(self):
+    def test_illegal_input_returns_minus_one(self):
         self.assertEqual(
             -1,
             checkout('K')
@@ -13,17 +13,26 @@ class TestCheckout(unittest.TestCase):
     def test_promo_applied_to_a(self):
         self.assertEqual(
             260,
-            checkout('AAAAAA')  # ?? 6A or 6 As?
+            checkout('AAAAAA')
         )
 
     def test_promo_applied_to_b(self):
-        # self.fail()
+        self.assertEqual(
+            90,
+            checkout('BB')
+        )
 
     def test_promo_applied_to_a_and_b(self):
-        # self.fail()
+        self.assertEqual(
+            175,
+            checkout('AAABB')
+        )
     
     def test_valid_input_returns_correct_amount(self):
-        # self.fail()
+        self.assertEqual(
+            130,
+            checkout('ABCDD')
+        )
 
 
 if __name__ == '__main__':
