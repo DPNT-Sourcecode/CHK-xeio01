917268caa5d366cedbbab2fba0279a1c8b007717
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 15:48:14 +0000

    Tue May 08 15:48:14 UTC 2018

---
+++ test/solutions/test_checkout.py
@@ -0,0 +1,14 @@
+import unittest
+
+from lib.solutions.checkout import checkout
+
+
+class TestCheckout(unittest.TestCase):
+    def test_illegal_input_returns_-one(self):
+        self.fail()
+
+    def test_a_promo
+
+
+if __name__ == '__main__':
+    unittest.main()
