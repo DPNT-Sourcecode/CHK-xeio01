d3982c069fc243c006c18628ad2efb5ba2c20694
sbx_user1059 <sbx_user1059@ip-10-12-173-151.eu-west-2.compute.internal>
2018-05-08 16:00:14 +0000

    Tue May 08 16:00:14 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -23,14 +23,24 @@ def checkout(skus):
         },
     }
 
-    # assuming it's 4A not AAAA
+    # assuming it's 4A not AAAA, and A comes in as 1A
 
     basket = {}
 
     for i in range(len(skus)):
-        if skus[i]
-        basket[skus[i+1]] = skus[i]
+        if skus[i] not in valid_skus:
+            return -1
+        if i%2 == 0:
+            basket[skus[i+1]] = skus[i]
     
     amount = 0
+
+    for item in basket:
+        if item in promotions:
+            non_promo_number = basket[item] % promotions[item]['multiplier']
+            amount += (
+                non_promo_number*prices[item] 
+                + (basket[item] - non_promo_number)*promotions[item]['amount']
+            )
 
     return -1
